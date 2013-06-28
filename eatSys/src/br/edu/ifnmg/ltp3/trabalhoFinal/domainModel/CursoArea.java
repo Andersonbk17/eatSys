38dc9e9aa31ae672ea13c582cffed458bf7b4ae4
lewandowsky <lewandowsky@localhost>
2013-06-28 20:04:50 -0300

    Criacao da Classe CursoArea

---
+++ eatSys/src/br/edu/ifnmg/ltp3/trabalhoFinal/domainModel/CursoArea.java
@@ -0,0 +1,87 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.ltp3.trabalhoFinal.domainModel;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author lewandowsky
+ */
+public class CursoArea {
+    private String nome;
+    private String sigla;
+    private boolean status;
+
+    public CursoArea(String nome, String sigla) {
+        this.nome = nome;
+        this.sigla = sigla;
+    }
+    
+    public CursoArea(){
+        this.nome = "";
+        this.sigla = "";
+        this.status = true;
+    
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.nome);
+        hash = 89 * hash + Objects.hashCode(this.sigla);
+        hash = 89 * hash + (this.status ? 1 : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final CursoArea other = (CursoArea) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.sigla, other.sigla)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return  nome + " - " + sigla  ;
+    }
+    
+    
+}
